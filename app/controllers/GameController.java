361595c4d3fb538dedb33e6406a3c4cc4955d2da
adavi <adavidmadsen@gmail.com>
2019-04-18 12:11:29 -0500

    update

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -1,43 +1,92 @@
 package controllers;
 
+import play.data.DynamicForm;
+import play.data.FormFactory;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
+
+import javax.inject.Inject;
 
 public class GameController extends Controller
 {
+    final private String CHARACTER_NAME_SESSION_KEY = "CHARACTER_NAME";
+    final private String CREW_MEMBERS_SESSION_KEY = "CREW_MEMBERS";
+
+    private FormFactory formFactory;
+
+
+    @Inject
+    public GameController(FormFactory formFactory)
+    {
+        this.formFactory = formFactory;
+    }
+
     public Result getWelcome()
     {
         return ok(views.html.welcome.render());
     }
 
-    public Result postStart()
+    public Result postStart(Http.Request request)
     {
-        return ok(views.html.start.render());
+        String crewMembers = "10";
+        DynamicForm form = formFactory.form().bindFromRequest(request);
+        String characterName = form.get("charactername");
+        return ok(views.html.start.render()).addingToSession(request, CHARACTER_NAME_SESSION_KEY, characterName)
+                .addingToSession(request, CREW_MEMBERS_SESSION_KEY, crewMembers);
     }
 
-    public Result postEastFromEngland()
+
+    public Result postEastFromEngland(Http.Request request)
     {
-        return ok(views.html.eastfromengland.render());
+        String crewSize = request.session().getOptional(CREW_MEMBERS_SESSION_KEY).orElse("0");
+        String newCrewSize = removeCrewMember(crewSize);
+
+
+        if (Integer.parseInt(newCrewSize) >= 5)
+        {
+            return ok(views.html.eastfromengland.render(newCrewSize)).addingToSession(request,
+                    CREW_MEMBERS_SESSION_KEY, newCrewSize);
+        }
+        else
+        {
+            return ok(views.html.oakisland.render());
+        }
     }
 
-    public Result postNorthEnd()
+    public Result postNorthEnd(Http.Request request)
     {
-        return ok(views.html.northend.render());
+        String characterName = request.session().getOptional(CHARACTER_NAME_SESSION_KEY).orElse("Unknown");
+        return ok(views.html.northend.render(characterName));
     }
 
-    public Result postWestFromEngland()
+    public Result postWestFromEngland(Http.Request request)
     {
-        return ok(views.html.westfromengland.render());
+        String crewSize = request.session().getOptional(CREW_MEMBERS_SESSION_KEY).orElse("0");
+        String newCrewSize = removeCrewMember(crewSize);
+
+
+        if (Integer.parseInt(newCrewSize) >= 5)
+        {
+            return ok(views.html.westfromengland.render(newCrewSize)).addingToSession(request,
+                    CREW_MEMBERS_SESSION_KEY, newCrewSize);
+        }
+        else
+        {
+            return ok(views.html.oakisland.render());
+        }
     }
 
-    public Result postEastEnd()
+    public Result postEastEnd(Http.Request request)
     {
-        return ok(views.html.eastend.render());
+        String characterName = request.session().getOptional(CHARACTER_NAME_SESSION_KEY).orElse("0");
+        return ok(views.html.eastend.render(characterName));
     }
 
-    public Result postWestEnd()
+    public Result postWestEnd(Http.Request request)
     {
-        return ok(views.html.westend.render());
+        String characterName = request.session().getOptional(CHARACTER_NAME_SESSION_KEY).orElse("Unknown");
+        return ok(views.html.westend.render(characterName));
     }
 
     public Result postHomePort()
@@ -49,4 +98,25 @@ public class GameController extends Controller
     {
         return ok(views.html.kittens.render());
     }
+
+    public Result postFirstNorthStop()
+    {
+        return ok(views.html.firstNorthStop.render());
+    }
+
+    public Result getStart()
+    {
+        return ok(views.html.start.render());
+    }
+
+    private String removeCrewMember(String currentCrew)
+    {
+        int crewMembers = Integer.parseInt(currentCrew);
+        int removeCrewMember = crewMembers - 1;
+
+        return Integer.toString(removeCrewMember);
+
+    }
+
+
 }
